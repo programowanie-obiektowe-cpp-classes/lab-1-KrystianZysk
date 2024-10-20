1088bcb79a07dea3ac5d97a58db1769fa6b3c281
KrystianZysk <zkrystian.z08@gmail.com>
2024-10-20 23:46:15 +0200

    Zadanie

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,15 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    double x = 0.0;
+    double y = 0.0;
+    public:
+    Wektor2D() {}
+    ~Wektor2D() {}
+    double getX() {return x;}
+    double getY() {return y;}
+    void setX(double x_) {x = x_;}
+    void setY(double y_) {y = y_;}
+    Wektor2D(double x_, double y_) {x = x_; y = y_;}
 };
+Wektor2D operator+(Wektor2D vec1, Wektor2D vec2) {return Wektor2D(vec1.getX() + vec2.getX(), vec1.getY() + vec2.getY());}
+double operator*(Wektor2D vec1, Wektor2D vec2) {return (vec1.getX() * vec2.getX()) + (vec1.getY() * vec2.getY());}
